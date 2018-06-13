897defef39aa1219c539c3ae744b5b864f19d142
oliviel <olivielultimate@gmail.com>
2018-06-12 23:57:33 -0400

    Add propstypes validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React from "react";
-import Header from "./Header";
-import Order from "./Order";
-import Inventory from "./Inventory";
-import sampleFishes from "../sample-fishes";
-import Fish from "./Fish";
-import base from "../base";
+import React from 'react';
+import PropTypes from 'prop-types'
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}  
   };
+
+  static propTypes = {
+    match: PropTypes.object
+  }
 
   componentDidMount() {
     const { params } = this.props.match;
